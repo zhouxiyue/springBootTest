aa26401237cebf499d5dda1203747b4c558f826b
zhouxiyue <15810683906@163.com>
2021-12-05 20:13:19 +0800

    add controller test

---
+++ demo/demo2/src/main/java/com/example/demo2/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
+}
